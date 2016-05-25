144c5e59a13b3c780399b16f44c77bc7769e5964
Fiques <romatill2@gmail.com>
2016-05-25 18:00:11 +0200

    Initial commit

---
+++ OnlineStore.WebUI.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OnlineStore.WebUI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
