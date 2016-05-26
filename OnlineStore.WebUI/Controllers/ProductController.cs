11af66e9073a0404dcbd055a77df339a12750c2e
Fiques <romatill2@gmail.com>
2016-05-26 05:47:08 +0200

    Added pagination

--- OnlineStore.WebUI/Controllers/ProductController.cs
+++ OnlineStore.WebUI/Controllers/ProductController.cs
@@ -1,24 +1,40 @@
 ﻿using OnlineStore.Domain.Abstract;
+using OnlineStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineStore.WebUI.HtmlHelpers;
 
 namespace OnlineStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         public readonly IProductRepository repository;
+        public int PageSize = 2;
         
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ OnlineStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
